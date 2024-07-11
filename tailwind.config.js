525b6cdda3a9558a42c188e926a7eeddd302e90a
Grentenville <arturtrentinibenedetti@gmail.com>
2024-07-11 16:02:20 -0300

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// npm install -D tailwindcss
+//npx tailwindcss init
+//npx tailwindcss -i ./css/input.css -o ./css/style.css --watch
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
