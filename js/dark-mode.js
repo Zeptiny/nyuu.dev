184ec64fd4780d6894761e2d041c1784390c194b
Grentenville <arturtrentinibenedetti@gmail.com>
2024-07-25 16:59:20 -0300

    Remove redundant js

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,21 +1,16 @@
+var turnstileWidget = document.getElementById('cf-turnstile');
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark');
+    themeToggleDarkIcon.classList.remove('hidden');
+    turnstileWidget.setAttribute('data-theme', 'light');
   } else {
     document.documentElement.classList.remove('dark');
-  }
-
-  var turnstileWidget = document.getElementById('cf-turnstile');
-  var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-  var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-  
-  // Change the icons inside the button based on previous settings
-  if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      themeToggleLightIcon.classList.remove('hidden');
-      turnstileWidget.setAttribute('data-theme', 'dark');
-  } else {
-      themeToggleDarkIcon.classList.remove('hidden');
-      turnstileWidget.setAttribute('data-theme', 'light');
+    themeToggleLightIcon.classList.remove('hidden');
+    turnstileWidget.setAttribute('data-theme', 'dark');
   }
   
   var themeToggleBtn = document.getElementById('theme-toggle');
