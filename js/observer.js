170944239aa7a28d8b08b18adbb10fa99e496715
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-05-27 13:43:45 -0300

    Add bottom-up animation

---
+++ js/observer.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.ease-animation');
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("opacity-100", "translate-y-0");
+        observer.unobserve(entry.target); // Optional: Stop observing after animation
+      }
+    });
+  }, { threshold: 0.2 });
+
+  elements.forEach(element => observer.observe(element));
+});
