f54dd6c5d5e97641fc9211b788e5abef7ff52c99
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-10-16 23:59:59 -0300

    fix: Cursor-default for everything unless explicitly noted

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <main>
+    <main className='cursor-default'>
       <HeroSection />
       <ServicesSection />
       <ProjectsSection />
