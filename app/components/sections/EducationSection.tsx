2c10c657c8b0360120af63bc6e110ff2ed44dd2d
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-10-20 11:36:52 -0300

    chore: Change download emoji to icon

--- app/components/sections/EducationSection.tsx
+++ app/components/sections/EducationSection.tsx
@@ -163,7 +163,7 @@ export default function EducationSection() {
                         download
                         className="btn btn-primary btn-sm"
                       >
-                        📄 {t.downloadCertificate}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg> {t.downloadCertificate}
                       </a>
                     </div>
                   )}
